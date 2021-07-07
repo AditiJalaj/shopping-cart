0c65abf738124a4e7790a37c4ec148becbb5d33d
ajalaj <aditi.jalaj@gmail.com>
2021-07-07 12:16:26 +0530

    refresh not working

--- src/components/PriceSummary.js
+++ src/components/PriceSummary.js
@@ -1,21 +1,29 @@
+import { useEffect, useState } from "react";
+
 const PriceSummary = ({  setCartVal,
     cartval,
     refresh,
     setRefresh}) => {
 
-       // console.log('price summ from p',cartval)
-        var totalprice=0;
-        var totalitem=0
-       cartval!==undefined &&  cartval.map((i)=>{
-            totalprice+=i.price
-            totalitem+=1;
-        })
+       //console.log('price summ from p',cartval)
+       const [totalprice,setTotalprice]=useState(0)
+       const [totalitem,setTotalitem]=useState(0)
+
+        useEffect(()=>{
+            console.log('useeffect from price summ ran')
+            const cart_arr=JSON.parse(localStorage.getItem('cart'))
+            cart_arr.length>0 && cart_arr.map((i)=>{
+                setTotalprice(pv=>pv+i.price)
+                setTotalitem(pv=>pv+i.item)
+            })
+            
+        },[refresh])
 
     return ( <div className="price-summary">
        <strong> PRICE SUMMARY </strong>
        <p style={{display:"flex",justifyContent:"space-between"}}>
        <span>Price ({totalitem} items)</span>
-       <span style={{fontWeight:'bolder',fontSize:"large"}}>{totalprice}$</span>
+       <span style={{fontWeight:'bolder',fontSize:"large"}}>{Math.floor(totalprice)}$</span>
        </p>
 
        <p  style={{display:"flex",justifyContent:"space-between"}}>
@@ -25,7 +33,7 @@ const PriceSummary = ({  setCartVal,
        
        <p style={{display:"flex",justifyContent:"space-between",borderTop:'1px solid black',marginTop:'5px'}}>
        <strong>TOTAL AMOUNT</strong>
-       <span>{totalprice}</span>
+       <span>{Math.floor(totalprice)}</span>
        </p>
        
         </div> );
--- src/components/cart/EachCart.js
+++ src/components/cart/EachCart.js
@@ -1,74 +1,107 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-const EachCart = ({brand,
-    title,
-    cartval,
-    setCartVal,
-    id,
-    image,
-    refresh,
-    setRefresh,
-    price}) => {
-        const [quan,setQuan]=useState(1)
-        const [updatedPrice,setUpdatedPrice]=useState(price)
+const EachCart = ({
+  brand,
+  title,
+  cartval,
+  setCartVal,
+  id,
+  image,
+  refresh,
+  setRefresh,
+  price,
+}) => {
+  const [quan, setQuan] = useState(1);
+  const [updatedPrice, setUpdatedPrice] = useState(price);
 
+  const updatepricelogic = (__id) => {
+    const found = cartval.find((i) => i.id === __id);
+    found.price = updatedPrice;
 
-        const inc=(_id)=>{
-            setQuan((pv)=>pv+1)
-            setUpdatedPrice((pv)=>pv+price)
-           
-            const found = cartval.find(i => i.id===_id);
-            found.price=updatedPrice;
-           //logic for setval to update price in ls
-        }
-
-        const dec=(_id)=>{
-           if(quan>1){
-            setQuan((pv)=>pv-1)
-            setUpdatedPrice((pv)=>pv-price)
-           
-            const found = cartval.find(i => i.id===_id);
-            found.price=updatedPrice;
-            //logic for setval
-           } 
-        }
-        
-        const remove=(_id)=>{
-           const confirmation= window.confirm("are you sure you want to remove item from cart?")
-            if(confirmation){
-                const removed=cartval && cartval.length>0 && cartval.filter((i)=>{
-                return i.id!==_id 
-            })
-        
-            setCartVal(localStorage.setItem('cart',JSON.stringify(removed))) 
-            setRefresh(!refresh)
-        }
-        }
-
-        
-    return ( <>
-        <div key={id} className="eachcart-item">
-        <p className="title">{title}</p>
-         <img src={image} className="eachcart-image" alt="cart-image" />
-      
-        <span className="brand">{brand}</span>
-     
-        <span style={{fontWeight:"bolder",fontSize:"30px",margin:"8px",padding:"2px"}}>
-        {updatedPrice}$</span>
-      
-
-      <button  onClick={() =>remove(id)} className="remove">
-       REMOVE
-      </button>
-      <button> SAVE FOR LATER </button>
-      <div>
-      <button onClick={()=>{dec(id)}}>-</button>
-      <span>{quan}</span>
-      <button onClick={()=>{inc(id)}}>+</button>
-      </div>
-    </div>
+    const restitems = cartval.filter((i) => i.id !== __id);
+    console.log("rest", restitems);
+    const merged = [...restitems, found];
     
-    </> );
-}
- 
+    //logic for setval to update price in ls
+    localStorage.setItem("cart", JSON.stringify(merged));
+    
+  };
+
+  const inc = (_id) => {
+    setQuan((pv) => pv + 1);
+    setUpdatedPrice((pv) => pv + price);
+    updatepricelogic(_id);
+    //setRefresh(!refresh) is not updating 
+  };
+
+  const dec = (_id) => {
+    if (quan > 1) {
+      setQuan((pv) => pv - 1);
+      setUpdatedPrice((pv) => pv - price);
+      updatepricelogic(_id);
+    }
+  };
+
+  const remove = (_id) => {
+    const confirmation = window.confirm(
+      "are you sure you want to remove item from cart?"
+    );
+    if (confirmation) {
+      const removed =
+        cartval &&
+        cartval.length > 0 &&
+        cartval.filter((i) => {
+          return i.id !== _id;
+        });
+
+      setCartVal(localStorage.setItem("cart", JSON.stringify(removed)));
+      setRefresh(!refresh);
+    }
+  };
+
+  return (
+    <>
+      <div key={id} className="eachcart-item">
+        <p className="title">{title}</p>
+        <img src={image} className="eachcart-image" alt="cart-image" />
+
+        <span className="brand">{brand}</span>
+
+        <span
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            margin: "8px",
+            padding: "2px",
+          }}
+        >
+          {updatedPrice}$
+        </span>
+
+        <button onClick={() => remove(id)} className="remove">
+          REMOVE
+        </button>
+        <button> SAVE FOR LATER </button>
+        <div>
+          <button
+            onClick={() => {
+              dec(id);
+            }}
+          >
+            -
+          </button>
+          <span>{quan}</span>
+          <button
+            onClick={() => {
+              inc(id);
+            }}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default EachCart;
